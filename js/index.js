957920c8d356b379d9832f985c6b70a44154b4ce
eduardo huencho <edubazinga1606rmail.com>
2021-04-19 17:25:50 -0400

    generar formulario y agregar js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+tinymce.init({
+    selector: '#descripcion-txt',
+    height: 200,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+    let nombre = document.querySelector("#nombre-txt").value;
+    let tipo = document.querySelector("#tipo-select").value;
+    let legendario = document.querySelector("#legendario-si").checked;
+    let desccripcion = tinymce.get("descripcion-txt").getContent ();
+    console.log("hola mundo",nombre, " ", tipo, " ", legendario," ", desccripcion);
+});
